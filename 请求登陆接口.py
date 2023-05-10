bc5337de72ded83be16f6015702c4f2159926776
huangshengquan <2506406292@qq.com>
2023-05-10 20:02:33 +0800

    登录接口已完成调试提取数据

--- 请求登陆接口.py
+++ 请求登陆接口.py
@@ -1,12 +1,19 @@
-import request
-from ru import yaml
+import requests
 import json
 
 def test_logintoken():
     host = "http://10.0.7.25/stage-api/auth/"
     url = host + "login"
     data = {
-        'usename': "LE02054",
+        "username": "LE02054",
         "password": "123456"
     }
-    
+    header = {
+        "Content-Type": "application/json",
+    }
+    rev_data = requests.post(headers=header, url=url, data=json.dumps(data))
+    print(json.loads(rev_data.content).get("data").get("access_token"))
+
+
+if __name__ == '__main__':
+    test_logintoken()
